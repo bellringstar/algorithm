231f45ececfd745f3652c31d4fab963011e7b903
bellring <starringbell@gmail.com>
2023-10-19 00:56:54 +0900

    java

---
+++ java/LC819.java
@@ -0,0 +1,10 @@
+public class LC819 {
+	class Solution {
+		public String mostCommonWord(String paragraph, String[] banned) {
+			// 가장 많이 등장한 단어(금지단어 제외)
+			// 1. paragraph를 전부 소문자로 변경해 단어들만 뽑아낸다.
+			// 2. 각 단어를 보면서 단어가 banned가 아니면 개수를 세서 추가한다.
+
+		}
+	}
+}
