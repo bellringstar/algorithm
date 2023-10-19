1193fd5fd87d465abf8cf055f494124e9ee0f0e6
김현종 <dw3510@naver.com>
2023-10-19 18:03:14 +0900

    java:

--- java/BOJ10825.java
+++ java/BOJ10825.java
@@ -4,56 +4,66 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class BOJ1759 {
+public class BOJ10825 {
+
+	static class Student {
+		private String name;
+		private int korean;
+		private int english;
+		private int math;
+
+		public Student(String name, int korean, int english, int math) {
+			this.name = name;
+			this.korean = korean;
+			this.english = english;
+			this.math = math;
+		}
+
+	}
+
 	static FastReader scan = new FastReader();
 	static StringBuilder sb = new StringBuilder();
-	static int N, M;
-	static char[] letters;
-	static int[] selected;
+	static int N;
+	static List<Student> studentList;
 
 	static void input() {
 		N = scan.nextInt();
-		M = scan.nextInt();
-		letters = new char[M+1];
-		for (int i = 1; i<=M; i++) letters[i] = scan.next().charAt(0);
-		Arrays.sort(letters, 1, M+1);
-		selected = new int[N+1];
-	}
-
-	static boolean isVowel(char x) {
-		return x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u';
-	}
-
-	static void rec_func(int k) {
-		if (k == N + 1) {
-			int vowel = 0, consonant = 0;
-			for (int i = 1; i <= N; i++) {
-				char x = letters[selected[i]];
-				if (isVowel(x)) vowel++;
-				else consonant++;
-			}
-			if (vowel >= 1 && consonant >= 2) {
-				for (int i = 1; i <= N; i++) sb.append(letters[selected[i]]);
-				sb.append('\n');
-			}
-		} else {
-			for (int cand = selected[k-1] + 1; cand <= M; cand++) {
-				selected[k] = cand;
-				rec_func(k+1);
-				selected[k] = 0;
-			}
+		studentList = new ArrayList<>(N);
+		for (int i = 0; i < N; i++) {
+			String[] s = scan.nextLine().split(" ");
+			Student student = new Student(s[0], Integer.parseInt(s[1])
+				, Integer.parseInt(s[2]), Integer.parseInt(s[3]));
+			studentList.add(student);
 		}
 	}
 
-
+	static void sortingList(List<Student> studentList) {
+		studentList.sort((s1, s2) -> {
+			if (s1.korean != s2.korean) {
+				return s2.korean - s1.korean; // 내림차순
+			}
+			if (s1.english != s2.english) {
+				return s1.english - s2.english; // 오름차순
+			}
+			if (s1.math != s2.math) {
+				return s2.math - s1.math; // 내맃마순
+			}
+			return s1.name.compareTo(s2.name);
+		});
+	}
 
 	public static void main(String[] args) {
 		input();
-		rec_func(1);
+		sortingList(studentList);
+		for (Student s : studentList) {
+			sb.append(s.name).append('\n');
+		}
 		System.out.println(sb.toString());
+
 	}
 
 	static class FastReader {
