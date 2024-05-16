6da622fec83140e568796124524b796370b54429
hyun <starringbell@gmail.com>
2024-05-16 13:49:38 +0900

    algo

--- solution/src/programmers/P67257.java
+++ solution/src/programmers/P67257.java
@@ -69,4 +69,79 @@ public class P67257 {
 			return 1;
 		}
 	}
+
+	/*
+    연산자 우선순위를 정한다. 연산자 3개 -> 3!가지
+    결과가 음수라면 절대값으로 변환
+    제출 점수가 가장 큰 참가자 우승
+    1. 연산자를 기준으로 분리한다.(연산자가 포함 돼 있어야한다.)
+    100,-,200,*,300,-,500,+,20
+    2. 총 6가지 우선순위를 순회하며 계산을 한다.
+    ex) +,-,* 순서 -> 순회하며 +를 만나면 계산 후 값을 그 위치에 넣는다...
+    3. 최대값을 갱신한다.(long)
+    */
+	class Solution2 {
+
+		static final List<String[]> precedences = new ArrayList<>();
+
+		public long solution(String expression) {
+			List<String> tokens = split(expression);
+			generatePre(0, "+-*".split(""), new boolean[3], new String[3]);
+
+			long answer = Long.MIN_VALUE;
+
+			for (String[] pre : precedences) {
+				long value = calculate(pre, new ArrayList<>(tokens));
+				answer = Math.max(answer, Math.abs(value));
+			}
+			return answer;
+		}
+
+		private long calculate(String[] pre, List<String> tokens) {
+			for (String op : pre) {
+				for (int i = 0; i < tokens.size(); i++) {
+					if (op.equals(tokens.get(i))) {
+						long a = Long.parseLong(tokens.get(i - 1));
+						long b = Long.parseLong(tokens.get(i + 1));
+						long result = 0;
+						switch (op) {
+							case "+" -> result = a + b;
+							case "-" -> result = a - b;
+							case "*" -> result = a * b;
+						}
+						tokens.remove(i - 1);
+						tokens.remove(i - 1);
+						tokens.remove(i - 1);
+						tokens.add(i - 1, Long.toString(result));
+						i -= 2;
+					}
+				}
+			}
+			return Long.parseLong(tokens.get(0));
+		}
+
+		private List<String> split(String expression) {
+			StringTokenizer tokens = new StringTokenizer(expression, "+-*", true);
+			List<String> words = new ArrayList<>();
+			while (tokens.hasMoreElements()) {
+				words.add(tokens.nextToken());
+			}
+			return words;
+		}
+
+		private void generatePre(int depth, String[] cand, boolean[] visit, String[] orders) {
+			if (depth == cand.length) {
+				precedences.add(orders.clone());
+				return;
+			}
+
+			for (int i = 0; i < cand.length; i++) {
+				if (visit[i]) continue;
+				orders[depth] = cand[i];
+				visit[i] = true;
+				generatePre(depth + 1, cand, visit, orders);
+				visit[i] = false;
+			}
+		}
+	}
 }
