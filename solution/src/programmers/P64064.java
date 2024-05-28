fe92891d79c6fd0704f2aeb6dcb78066cb80bf90
hyun <starringbell@gmail.com>
2024-05-28 15:16:07 +0900

    P64064 solution

--- solution/src/programmers/P64064.java
+++ solution/src/programmers/P64064.java
@@ -1,6 +1,7 @@
 package programmers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -61,6 +62,35 @@ public class P64064 {
 			recFunc(0, new HashSet<>());
 
 			return idSet.size();
+		}
+	}
+
+	class Solution2 {
+
+		private void count(int index, Set<String> banned, String[][] bans, Set<Set<String>> banSet) {
+			if (index == bans.length) {
+				banSet.add(new HashSet<>(banned));
+				return;
+			}
+
+			for (String id : bans[index]) {
+				if (banned.contains(id)) continue;
+				banned.add(id);
+				count(index + 1, banned, bans, banSet);
+				banned.remove(id);
+			}
+		}
+
+		public int solution(String[] user_id, String[] banned_id) {
+
+			String[][] bans = Arrays.stream(banned_id)
+					.map(banned -> banned.replace("*", "."))
+					.map(banned -> Arrays.stream(user_id)
+							.filter(id -> id.matches(banned))
+							.toArray(String[]::new))
+					.toArray(String[][]::new);
+
+
 		}
 	}
 }
