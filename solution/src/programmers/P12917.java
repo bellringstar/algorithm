ddd3816b4958d6ba76a8a4196114903067c7d898
hyun <starringbell@gmail.com>
2024-05-30 16:23:52 +0900

    algo

--- solution/src/programmers/P12917.java
+++ solution/src/programmers/P12917.java
@@ -1,8 +1,19 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class P12917 {
 
 	class Solution {
+		public String solution(String s) {
+			return Arrays.stream(s.split(""))
+					.sorted((s1, s2) -> s2.compareTo(s1))
+					.collect(Collectors.joining(""));
+		}
+	}
+
+	class Solution2 {
 		public String solution(String s) {
 
 			return s.chars()
