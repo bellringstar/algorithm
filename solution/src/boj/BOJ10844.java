57fd71f5208b06a3f9166bd3210602ff6caa787a
bellringstar <bellringstar@gmail.com>
2024-08-10 16:11:00 +0900

    algo

--- solution/src/boj/BOJ10844.java
+++ solution/src/boj/BOJ10844.java
@@ -5,25 +5,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ11726 {
+public class BOJ10844 {
 
     static FastReader scan = new FastReader();
     static int N;
-    static int[] mem;
+    static int[][] mem;
 
     static void input() {
         N = scan.nextInt();
-        mem = new int[1002];
+        mem = new int[101][10]; // [길이][마지막 수 0~9]
+    }
+
+    static void pro() {
+        for (int i = 1; i <= 9; i++) {
+            mem[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            mem[i][0] = mem[i - 1][1];
+            for (int j = 1; j <= 8; j++) {
+                mem[i][j] = (mem[i - 1][j - 1] + mem[i - 1][j + 1]) % 1_000_000_000;
+            }
+            mem[i][9] = mem[i - 1][8];
+        }
+
+        long count = 0;
+        for (int i = 0; i <= 9; i++) {
+            count = (count + mem[N][i]) % 1_000_000_000;
+        }
+        System.out.println(count);
     }
 
     public static void main(String[] args) {
         input();
-        mem[1] = 1;
-        mem[2] = 2;
-        for (int i = 3; i <= N; i++) {
-            mem[i] = (mem[i-1] + mem[i-2]) % 10007;
-        }
-        System.out.println(mem[N]);
+        pro();
     }
 
     static class FastReader {
